2f3c2c141352bdd4400e6163c49ac955c6686086
josekameydev01 <gk01cracklitos@gmail.com>
2025-09-07 11:03:22 -0600

    first commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by carlosgalvankamey on 9/7/25.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
