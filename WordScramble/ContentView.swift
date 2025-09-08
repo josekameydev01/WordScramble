4a3d1b1c6979a8639c7513d01c69073648cb7423
josekameydev01 <gk01cracklitos@gmail.com>
2025-09-07 20:32:34 -0600

    creating sections in our lists

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -10,10 +10,18 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         List {
-            Text("Static row 1")
-            Text("Static row 2")
-            ForEach(0..<5) {
-                Text("Dynamic row \($0)")
+            Section("Section 1") {
+                Text("Static row 1")
+                Text("Static row 2")
+            }
+            Section("Section 2") {
+                ForEach(0..<5) {
+                    Text("Dynamic row \($0)")
+                }
+            }
+            Section("Section 3") {
+                Text("Static row 3")
+                Text("Static row 4")
             }
         }
     }
