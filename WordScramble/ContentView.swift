60279e6ed7d53946e943924f5ad774d0e709bd7a
josekameydev01 <gk01cracklitos@gmail.com>
2025-09-07 21:11:18 -0600

    how to create a list from an array

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -8,23 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    private let users = ["Carlos", "Cracklitos", "Carlitos"]
     var body: some View {
-        List {
-            Section("Section 1") {
-                Text("Static row 1")
-                Text("Static row 2")
-            }
-            Section("Section 2") {
-                ForEach(0..<5) {
-                    Text("Dynamic row \($0)")
-                }
-            }
-            Section("Section 3") {
-                Text("Static row 3")
-                Text("Static row 4")
-            }
+        List(users, id: \.self) {
+            Text($0)
         }
-        .listStyle(.grouped)
     }
 }
 
