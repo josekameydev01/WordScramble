7d91e908a621a36dc3ffabae48d724563fa589cb
josekameydev01 <gk01cracklitos@gmail.com>
2025-09-08 23:18:47 -0600

    loading resources from our app bundle

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -8,11 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
-    private let users = ["Carlos", "Cracklitos", "Carlitos"]
+    @State private var alertTitle = "Error"
+    @State private var alertMessage = "The file cannot be found"
+    @State private var showAlert = false
+    
     var body: some View {
-        List(users, id: \.self) {
-            Text($0)
+        VStack {
+            Button("Show Message", action: showMessage)
+                .buttonStyle(.borderedProminent)
         }
+        .alert(alertTitle, isPresented: $showAlert) {
+            Button("OK") { }
+        } message: {
+            Text(alertMessage)
+        }
+    }
+    private func showMessage() {
+        showAlert = true
+        guard let fileURL = Bundle.main.url(forResource: "file", withExtension: "txt") else {
+            return
+        }
+        
+        alertTitle = "file content"
+        alertMessage = (try? String(contentsOf: fileURL, encoding: .utf8)) ?? "The file couldn't be read"
     }
 }
 
